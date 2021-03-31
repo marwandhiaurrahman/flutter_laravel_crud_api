eff191b145c9462d6460a26e7c8da33d04553444
Marwan Dhiaur Rahman <45331803+marwandhiaurrahman@users.noreply.github.com>
2021-03-31 09:19:07 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Future<Widget> loadFromFuture() async {
+    // <fetch data from server. ex. login>
+
+    return Future.value(new Login());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+        seconds: 3,
+        navigateAfterSeconds: new Login(),
+        // navigateAfterFuture: loadFromFuture(),
+        title: new Text(
+          'Welcome In SplashScreen',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+        ),
+        image: new Image.network('https://i.imgur.com/TyCSG9A.png'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 100.0,
+        onClick: () => print("Flutter Egypt"),
+        loaderColor: Colors.blue);
+  }
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Welcome In SplashScreen Package"),
+          automaticallyImplyLeading: false),
+      body: new Center(
+        child: new Text(
+          "Done!",
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+        ),
+      ),
+    );
+  }
+}
